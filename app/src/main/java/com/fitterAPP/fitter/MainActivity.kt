e1ca204506bb0270c77c7a602b31e858b0db5d3a
Mene-hub <menegotto.claudio@gmail.com>
2022-05-04 19:18:13 +0200

    first Modal Bottom Sheet implementation

--- app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
+++ app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.fitterAPP.fitter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ttMenu : LinearLayout;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ttMenu = findViewById(R.id.LL_Profile_menu)
+        ttMenu?.setOnClickListener {
+            val modalBottomSheet = profileMenu()
+            modalBottomSheet.show(supportFragmentManager, profileMenu.TAG)
+        }
     }
 }
