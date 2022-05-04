9bcafc5ba3323c5bb197f69b8187dc07ab14edec
Code-Dani <satrianodaniel@gmail.com>
2022-05-04 17:22:18 +0200

    Added childEventListener

--- app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
+++ app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
@@ -2,10 +2,47 @@ package com.fitterAPP.fitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView : RecyclerView
+    private lateinit var user : Athlete
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RealTimeDBHelper.readToDoItems(getAthleteEventListener())
+
+
     }
+
+    private fun getAthleteEventListener(): ChildEventListener {
+        val childEventListener = object : ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        return childEventListener
+    }
+
 }
