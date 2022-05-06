7b74fa82e989a370f9ee38e0bc35f51da1a3ee58
Code-Dani <satrianodaniel@gmail.com>
2022-05-06 22:14:12 +0200

    Rimossi import inutilizzati

--- app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
+++ app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
@@ -8,9 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
