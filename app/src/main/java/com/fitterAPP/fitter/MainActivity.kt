942b9a4ce1cecaa4588dd40c57b2d59f5da831f2
Code-Dani <satrianodaniel@gmail.com>
2022-05-06 21:28:59 +0200

    Made fake UI for the Login/Registration/Signout via Firebase. Working properly now.

--- app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
+++ app/src/main/java/com/fitterAPP/fitter/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -14,6 +16,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val TAG_login : String = "MainActivity-Login"
+
     private lateinit var listView : RecyclerView
     private lateinit var user : Athlete
 
@@ -31,11 +35,17 @@ class MainActivity : AppCompatActivity() {
         //istantiate auth variable
         auth = Firebase.auth
 
-        val tmpButtonLogin : Button? = null
-        tmpButtonLogin?.setOnClickListener(loginEmailPSW("satrianodaniel@gmail.com","123"))
+        //TMP BUTTONS
+        val tmpButtonLogin : Button = findViewById(R.id.btn_login)
+        tmpButtonLogin.setOnClickListener(loginEmailPSW())
 
-        val tmpButtonRegister : Button? = null
-        tmpButtonRegister?.setOnClickListener(createUser("satrianodaniel@gmail.com","123"))
+        val tmpButtonRegister : Button = findViewById(R.id.btn_register)
+        tmpButtonRegister.setOnClickListener(createUser())
+
+        val tmpButtonSignOut : Button = findViewById(R.id.btn_signout)
+        tmpButtonSignOut.setOnClickListener{
+            auth.signOut()
+        }
     }
 
     override fun onStart() {
@@ -44,25 +54,33 @@ class MainActivity : AppCompatActivity() {
         val currentUser = auth.currentUser
         if(currentUser != null) {
             //updateUI(currentUser) UPDATE UI ACCORDINGLY
+            findViewById<TextView>(R.id.status).text =  auth.currentUser?.uid.toString()
+            Log.w(TAG_login, auth.currentUser?.email.toString())
         }else{
             //USER NOT LOGGED IN - needs to login
         }
     }
 
     //LOGIN VIA EMAIL AND PASSWORD
-    private fun loginEmailPSW(email: String, password: String): View.OnClickListener? {
+    private fun loginEmailPSW(): View.OnClickListener? {
         val listener = View.OnClickListener {
+            val email : String = findViewById<EditText>(R.id.et_email).text.toString()
+            val password : String = findViewById<EditText>(R.id.et_password).text.toString()
+
             auth.signInWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this){ task ->
                     if(task.isSuccessful){
                         // Sign in success, update UI with the signed-in user's information
-                        Log.d("MainActivity",  "Login success")
+                        Log.d(TAG_login,  "Login success")
                         val user = auth.currentUser
                         //updateUI(user) UPDATE UI ACCORDINGLY
+
+                        findViewById<TextView>(R.id.status).text = auth.currentUser?.uid.toString()
                     }else {
                         // If sign in fails, display a message to the user.
-                        Log.w("MainActivity", "Login failed", task.exception)
+                        Log.w(TAG_login, "Login failed", task.exception)
                         //USER DOESN'T LOGIN FAIL, CREATE ACCOUNT
+                        findViewById<TextView>(R.id.status).text = "ACCOUNT NOT FOUND, CREATE NEW ONE"
                     }
                 }
         }
@@ -70,16 +88,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     //IF USER NOT FOUND IN "loginEmailPSW" THEN CREATES ONE
-    private fun createUser(email: String, password: String) : View.OnClickListener {
+    private fun createUser() : View.OnClickListener {
         val listener = View.OnClickListener {
+            val email : String = findViewById<EditText>(R.id.et_email).text.toString()
+            val password : String = findViewById<EditText>(R.id.et_password).text.toString()
+
             auth.createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this){ task ->
                     if(task.isSuccessful){
                         // Sign in success, update UI with the signed-in user's information
-                        Log.d("MainActivity", "User creation: OK")
+                        Log.d(TAG_login, "User creation: OK")
+
+                        findViewById<TextView>(R.id.status).text =  auth.currentUser?.uid.toString()
                     }else {
                         // If sign in fails, display a message to the user.
-                        Log.w("MainActivity", "User creation: FAILED", task.exception)
+                        Log.w(TAG_login, "User creation: FAILED", task.exception)
                         finish()
                     }
                 }
