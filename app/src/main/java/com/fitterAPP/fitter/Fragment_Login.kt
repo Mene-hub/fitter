d4d1dc2713bf55f56d07e6408d7de980a38708e0
Code-Dani <satrianodaniel@gmail.com>
2022-05-08 22:25:07 +0200

    Aggiunti pulsanti login facebook/google

--- app/src/main/java/com/fitterAPP/fitter/Fragment_Login.kt
+++ app/src/main/java/com/fitterAPP/fitter/Fragment_Login.kt
@@ -7,7 +7,6 @@ import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
@@ -21,49 +20,55 @@ class Fragment_Login : Fragment() {
     private val TAG_login : String = "LoginActivity-Login"
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var intent : Intent    //MainWindow caller
+    private lateinit var binding : FragmentLoginBinding //Binding
 
-    private lateinit var loginButton : Button
-    private lateinit var registerButton : Button
-
-    private lateinit var intent : Intent
-    private lateinit var psw_text_layout : TextInputLayout
-
-    private lateinit var binding : FragmentLoginBinding
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         //istantiate auth variable
         auth = Firebase.auth
 
         //BUTTONS for login/register
-        loginButton = binding.btnLogin
-        loginButton.setOnClickListener(loginEmailPSW())
+        binding.btnLogin.setOnClickListener(loginEmailPSW())
+        binding.IVLoginGoogle.setOnClickListener(loginGoogle())
+        binding.IVLoginFacebook.setOnClickListener(loginFacebook())
 
         //ERRORE PER PASSWORD SBAGLIATA / EMAIL SBAGLIATA
-        psw_text_layout = binding.etLoginPasswordLayout
-        val psw_editText = binding.etLoginPassword
-        psw_editText.doOnTextChanged { text, start, before, count ->
+        val psw_text_layout : TextInputLayout = binding.etLoginPasswordLayout
+        binding.etLoginPassword.doOnTextChanged { text, start, before, count ->
             if(psw_text_layout.error != null) {
                 psw_text_layout.error = null
             }
         }
-
         //EVENTO PER CONTROLLARE SE L'EMAIL INSERITA E' CORRETTA
-        binding.etLoginEmail.setOnFocusChangeListener{
-            _, focused ->
+        binding.etLoginEmail.setOnFocusChangeListener{ _, focused ->
             if(!focused){
                 binding.etLoginEmailLayout.helperText = validEmail()
             }
         }
-
         //INTENT PER APRIRE MAIN WINDOW
         intent = Intent(requireActivity(), MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
 
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun loginGoogle(): View.OnClickListener {
+        val listener = View.OnClickListener {
+
+        }
+        return listener
+    }
+
+    private fun loginFacebook(): View.OnClickListener {
+        val listener = View.OnClickListener {
+
+        }
+        return listener
     }
 
     private fun validEmail(): String? {
@@ -112,7 +117,7 @@ class Fragment_Login : Fragment() {
                         // If sign in fails, display a message to the user.
                         Log.w(TAG_login, "Login failed", task.exception)
 
-                        psw_text_layout.error = getString(R.string.password_incorrect)
+                        binding.etLoginPasswordLayout.error = getString(R.string.password_incorrect)
                         Toast.makeText(requireActivity().baseContext, getString(R.string.password_incorrect), Toast.LENGTH_LONG).show()
                     }
             }
