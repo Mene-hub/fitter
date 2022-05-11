253c7db1e70830a0b204745722d248efccd85d7a
Code-Dani <satrianodaniel@gmail.com>
2022-05-11 18:24:05 +0200

    Fixato problema

--- app/src/main/java/com/fitterAPP/fitter/FragmentControlers/Fragment_Profile.kt
+++ app/src/main/java/com/fitterAPP/fitter/FragmentControlers/Fragment_Profile.kt
@@ -6,8 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class MyFitnessCards : Fragment() {
-
+class profile : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
