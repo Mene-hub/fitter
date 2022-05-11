4efeb90bef937acb1ddaa6bd796ab45622aba8dd
Code-Dani <satrianodaniel@gmail.com>
2022-05-11 18:35:13 +0200

    sisteamto

--- app/src/main/java/com/fitterAPP/fitter/FragmentControlers/Fragment_Profile.kt
+++ app/src/main/java/com/fitterAPP/fitter/FragmentControlers/Fragment_Profile.kt
@@ -13,7 +13,7 @@ class profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_findprofile, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }
