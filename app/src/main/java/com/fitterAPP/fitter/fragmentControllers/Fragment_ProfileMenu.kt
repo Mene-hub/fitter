5305742c2547dd35684867de622708ce555aaf4c
Code-Dani <satrianodaniel@gmail.com>
2022-05-28 23:35:28 +0200

    email verification implemented

--- app/src/main/java/com/fitterAPP/fitter/fragmentControllers/Fragment_ProfileMenu.kt
+++ app/src/main/java/com/fitterAPP/fitter/fragmentControllers/Fragment_ProfileMenu.kt
@@ -5,13 +5,15 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.navigation.fragment.findNavController
 import com.fitterAPP.fitter.MainActivity
 import com.fitterAPP.fitter.R
 import com.fitterAPP.fitter.databinding.FragmentProfileMenuBinding
-
-const val ARG_ITEM_COUNT = "item_count"
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class profileMenu : BottomSheetDialogFragment() {
     private lateinit var binding : FragmentProfileMenuBinding
@@ -22,19 +24,49 @@ class profileMenu : BottomSheetDialogFragment() {
         val logoutbt : AppCompatTextView = binding.txtLogout
         val recapbt : AppCompatTextView = binding.txtRecap
 
+        val verifyEmail : AppCompatTextView = binding.txtVerifyEmail
+        val linearLayoutVerifyEmail : LinearLayout = binding.LLVerifyEmail
 
+        if(checkIfShouldDisplay() != true){
+            linearLayoutVerifyEmail.visibility = View.VISIBLE
+            verifyEmail.setOnClickListener(verificationProcess())
+        }else{
+            linearLayoutVerifyEmail.visibility = View.GONE
+        }
 
         logoutbt.setOnClickListener {
             (activity as MainActivity).logout()
             dismiss()
         }
 
-        recapbt.setOnClickListener( View.OnClickListener {
+        recapbt.setOnClickListener {
             findNavController().navigate(R.id.action_myFitnessCards_to_timeRacap)
             dismiss()
-        })
+        }
 
         return binding.root
+    }
+
+    private fun verificationProcess(): View.OnClickListener {
+        val listener = View.OnClickListener {
+            Firebase.auth.currentUser?.sendEmailVerification()?.addOnCompleteListener{ task ->
+                if(task.isSuccessful){
+                    Toast.makeText(requireContext(),"Email sent successfully", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(requireContext(),task.exception?.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+            dismiss()
+        }
+        return listener
+    }
+
+    fun checkIfShouldDisplay() : Boolean?{
+        Firebase.auth.currentUser?.reload()
+        if(Firebase.auth.currentUser?.providerId == "Facebook"){
+            return true
+        }
+        return Firebase.auth.currentUser?.isEmailVerified
     }
 
 
