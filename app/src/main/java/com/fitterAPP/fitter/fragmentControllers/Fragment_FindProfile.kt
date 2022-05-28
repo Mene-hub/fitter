afd1661c0f603bd93de06fe0b9cd0973c2e1af86
Code-Dani <satrianodaniel@gmail.com>
2022-05-28 13:51:06 +0200

    Implementata recycler view per i suggerimenti della ricerca

--- app/src/main/java/com/fitterAPP/fitter/fragmentControllers/Fragment_FindProfile.kt
+++ app/src/main/java/com/fitterAPP/fitter/fragmentControllers/Fragment_FindProfile.kt
@@ -1,5 +1,6 @@
 package com.fitterAPP.fitter.fragmentControllers
 
+import android.app.SearchManager
 import com.fitterAPP.fitter.R
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.SearchView
 import android.widget.SimpleCursorAdapter
 import androidx.lifecycle.LifecycleOwner
@@ -14,9 +16,13 @@ import androidx.lifecycle.lifecycleScope
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.fitterAPP.fitter.MainActivity
 import com.fitterAPP.fitter.classes.Athlete
 import com.fitterAPP.fitter.databases.RealTimeDBHelper
 import com.fitterAPP.fitter.databinding.FragmentFindprofileBinding
+import com.fitterAPP.fitter.itemsAdapter.FitnessCardAdapter
+import com.fitterAPP.fitter.itemsAdapter.SuggestionAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.database.*
@@ -24,6 +30,8 @@ import com.google.firebase.database.*
 class FindProfile : Fragment() {
     private lateinit var binding : FragmentFindprofileBinding
     private var databaseReference: DatabaseReference = FirebaseDatabase.getInstance(RealTimeDBHelper.getDbURL()).getReference("USERS")
+    private var suggestedUsers : MutableList<Athlete> = ArrayList()
+    private lateinit var adapter : SuggestionAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentFindprofileBinding.inflate(inflater,container,false)
@@ -33,9 +41,19 @@ class FindProfile : Fragment() {
         binding.SVFindUsers.setOnQueryTextListener(queryTextListener())
 
 
+        adapter = context?.let { SuggestionAdapter((activity as MainActivity), suggestedUsers) }!!
+        binding.recyclerView.adapter = adapter
+
         return binding.root
     }
 
+
+    /**
+     * * Private listener for the SearchView which updates on every change in the textfield or when submit is pressed.
+     * In particular onQUeryTextChange is the method used for the research in the database which sends an input to databaseQuery(textString) whenever textString is not Blank.<br />
+     * @see databaseQuery for more information about that method
+     * @author Daniel Satriano
+     */
     private fun queryTextListener(): SearchView.OnQueryTextListener {
         val listener = object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -53,30 +71,45 @@ class FindProfile : Fragment() {
         return listener
     }
 
+
+    /**
+     * * databaseQuery is a function that is called in queryTextListener() and it queries the database for the particular string given as input (text : String?).
+     * * for more information about the database query method check the [documentation](https://firebase.google.com/docs/database/android/lists-of-data#sorting_and_filtering_data)
+     * * This function also has inside a child listener which will get updates everytime it finds new child that have inside the given string
+     * @see queryTextListener for more information about that function
+     * @author Daniel Satriano
+     */
     fun databaseQuery(text : String?){
-        val usernameOrdered : Query  = databaseReference.orderByChild("username").startAt(text).endAt("$text\uF7FF") .limitToFirst(20)
+        val usernameOrdered : Query  = databaseReference.orderByChild("username").startAt(text).endAt("$text\uF7FF") .limitToFirst(10)
         Log.d("FindProfile", "---------------------")
+        suggestedUsers.clear()
+        adapter.notifyDataSetChanged()
         usernameOrdered.addChildEventListener(object : ChildEventListener{
 
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
                 val item = snapshot.getValue(Athlete::class.java)
-                Log.d("FindProfile", item?.username.toString())
 
+                suggestedUsers.add(item!!)
+
+                adapter.notifyItemChanged(suggestedUsers.indexOf(item))
+                Log.d("FindProfile", item.username.toString())
+                Log.d("FindProfile", suggestedUsers.count().toString())
             }
 
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d("FindProfile", "entro changed")
             }
-
             override fun onChildRemoved(snapshot: DataSnapshot) {
+                Log.d("FindProfile", "entro removed")
             }
-
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
             }
-
             override fun onCancelled(error: DatabaseError) {
+                Log.d("FindProfile", "entro cancelled")
             }
 
         })
+
     }
 
     private fun bottomNavItemSelected(): NavigationBarView.OnItemSelectedListener {
---
+++ app/src/main/java/com/fitterAPP/fitter/itemsAdapter/SuggestionAdapter.kt
@@ -0,0 +1,59 @@
+package com.fitterAPP.fitter.itemsAdapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.fitterAPP.fitter.R
+import com.fitterAPP.fitter.classes.Athlete
+import com.squareup.picasso.Picasso
+
+/**
+ * @author DanielSatriano
+ */
+class SuggestionAdapter(val context2: Context, val users:MutableList<Athlete>) : RecyclerView.Adapter<SuggestionAdapter.Holder>() {
+
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val username : TextView = itemView.findViewById(R.id.TV_suggestionText)
+        val image : ImageView = itemView.findViewById(R.id.IV_profileImage)
+
+        fun setCard(user:Athlete){
+            username.text = user.username
+            if(user.profilePic != null || user.profilePic != "")
+                Picasso.get().load(user.profilePic).resize(100, 100).centerCrop().into(image)
+            else
+                image.setBackgroundResource(R.drawable.person_24)
+
+            itemView.setOnClickListener {
+                Toast.makeText(itemView.context, username.text.toString(), Toast.LENGTH_LONG).show()
+
+                /*val action : NavDirections = MyFitnessCardsDirections.actionMyFitnessCardsToFragmentShowCardDialog(Card)
+                it.findNavController().navigate(action)*/
+            }
+
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view: View = LayoutInflater.from(context2).inflate(R.layout.suggestion_adapter_row, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val user: Athlete = users[position]
+        holder.setCard(user)
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+
+
+}
