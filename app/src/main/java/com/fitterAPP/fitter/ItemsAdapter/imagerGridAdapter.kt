fe3e0de2157628eb9b0f27ee79011f9f00b81683
Code-Dani <satrianodaniel@gmail.com>
2022-06-02 19:05:44 +0200

    Sistemato il radio button, adesso quando clicco su un secondo radio button il primo si disabilita

--- app/src/main/java/com/fitterAPP/fitter/ItemsAdapter/imagerGridAdapter.kt
+++ app/src/main/java/com/fitterAPP/fitter/ItemsAdapter/imagerGridAdapter.kt
@@ -11,34 +11,42 @@ import com.fitterAPP.fitter.R
 
 class imagerGridAdapter(val context2: Context, val images : MutableList<Int>) : RecyclerView.Adapter<imagerGridAdapter.Holder>() {
 
-    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var radioButton : RadioButton? = null
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         var bgImage : ImageView = itemView.findViewById(R.id.bgImage)
         var card : CardView = itemView.findViewById(R.id.cardbg)
-        var sel : RadioButton = itemView.findViewById(R.id.selected_rb)
-
+        var radioButton : RadioButton = itemView.findViewById(R.id.selected_rb)
 
         fun setCard(id: Int){
-
             bgImage.setImageResource(id)
-            itemView.setOnClickListener {
-                sel.isChecked = true
+            bgImage.setOnClickListener{
+                //checked radio button
+                radioButton.isChecked = true
             }
-
-            //se non è più selezionato deve deselezionarsi
-
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-            val view: View = LayoutInflater.from(context2).inflate(R.layout.item_image_list, parent, false)
-            return Holder(view)
+        val view: View = LayoutInflater.from(context2).inflate(R.layout.item_image_list, parent, false)
+        return Holder(view)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val image: Int = images[position]
         holder.setCard(image)
+
+        //Set listener on radio button
+        holder.radioButton.setOnCheckedChangeListener{ _, isChecked ->
+            //if radioButton != holder.radioButton -> radioButton.isChecked = false
+            if(radioButton != holder.radioButton) {
+                radioButton?.isChecked = false
+            }
+            if (isChecked) {
+                radioButton = holder.radioButton
+            }
+        }
     }
 
     override fun getItemCount(): Int {
