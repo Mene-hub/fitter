b7ad7967b3cca87d1c0b230ac4739849c7cf9dbf
Code-Dani <satrianodaniel@gmail.com>
2022-07-01 11:50:29 +0200

    Sistemato username che adesso ammette al massimo un punto e un trattino basso

--- app/src/main/java/com/fitterAPP/fitter/fragmentControllers/Fragment_Profile.kt
+++ app/src/main/java/com/fitterAPP/fitter/fragmentControllers/Fragment_Profile.kt
@@ -74,18 +74,32 @@ class Profile : Fragment() {
     private fun updateProfileListener(): View.OnClickListener {
         val listener = View.OnClickListener {
 
-            Athlete.username = binding.etUsername.text.toString()
-            //Athlete.profilePic = URL
-            Athlete.profileBio = binding.etBio.text.toString()
+            val check1 = binding.etUsername.text.toString().split("_").count()
+            val check2 = binding.etUsername.text.toString().split(".").count()
 
-            if(etUsername.text.isNotBlank()){
+            if(check1 <= 2 && check2 <= 2) {
+                Athlete.username = binding.etUsername.text.toString()
 
-                val athlete = Athlete(Athlete.UID,Athlete.username,Athlete.profilePic,Athlete.profileBio,Athlete.spotifyplayList)
-                StaticAthleteDatabase.setAthleteItem(dbReference, Athlete.UID, athlete)
+                //Athlete.profilePic = URL
+                Athlete.profileBio = binding.etBio.text.toString()
+
+                if (etUsername.text.isNotBlank()) {
+
+                    val athlete = Athlete(
+                        Athlete.UID,
+                        Athlete.username,
+                        Athlete.profilePic,
+                        Athlete.profileBio,
+                        Athlete.spotifyplayList
+                    )
+                    StaticAthleteDatabase.setAthleteItem(dbReference, Athlete.UID, athlete)
+                }
+
+                Toast.makeText(requireContext(), "Profile updated", Toast.LENGTH_LONG).show()
+                findNavController().navigate(R.id.action_profile_to_myFitnessCards)
+            }else{
+                binding.etUsername.error = "Username can only contain one '.' or one '_'"
             }
-
-            Toast.makeText(requireContext(),"Profile updated", Toast.LENGTH_LONG).show()
-            findNavController().navigate(R.id.action_profile_to_myFitnessCards)
         }
         return listener
     }
@@ -99,31 +113,6 @@ class Profile : Fragment() {
         username.setText(Athlete.username)
         bio.setText(Athlete.profileBio)
         email.setText(auth.currentUser?.email)
-    }
-
-
-
-
-    private fun grabImageFromDisk(){
-        if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-            //SELEZIONA IMMAGINE
-
-        }else{
-            //CODICE REQUEST_CODE SERVE A IDENTIFICARE LA RISPOSTA CHE MI DARà IL COMPAT
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE)
-        }
-    }
-
-
-    @Deprecated("Deprecated in Java", ReplaceWith("super.onRequestPermissionsResult(requestCode, permissions, grantResults)", "androidx.fragment.app.Fragment"))
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if(requestCode == REQUEST_CODE){
-            grabImageFromDisk()
-        }else{
-            Toast.makeText(requireContext(), "Oops, you just denied the permission for storage. You can also allow it from settings.", Toast.LENGTH_LONG).show()
-        }
     }
 
 }
