d3fe61bbc766e23c57ca0f7857692b2ba0c8632b
Code-Dani <satrianodaniel@gmail.com>
2022-07-23 19:00:15 +0200

    Sistemato lo swipe e aggiunte le label. SwipeGesture.kt

--- app/src/main/java/com/fitterAPP/fitter/classes/SwipeGesture.kt
+++ app/src/main/java/com/fitterAPP/fitter/classes/SwipeGesture.kt
@@ -2,21 +2,29 @@ package com.fitterAPP.fitter.classes
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Typeface
+import android.util.TypedValue
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.fitterAPP.fitter.R
 import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 
 /**
+ * Class used to manage swipes and movements of listItems inside the exercise recyclerview
  * @author Daniel Satriano
+ * @since 23/07/2022
+ * @param context context of the caller
  */
 abstract class SwipeGesture(context : Context) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
 
-    val deleteColor = ContextCompat.getColor(context, android.R.color.holo_red_dark)
-    val recapColor = ContextCompat.getColor(context, android.R.color.holo_green_dark)
+    val deleteColor = ContextCompat.getColor(context, R.color.red)
+    val recapColor = ContextCompat.getColor(context, R.color.green)
     val deleteIcon = android.R.drawable.ic_menu_delete
     val recapIcon = R.drawable.ic_check
+
+    val typeface = ResourcesCompat.getFont(context, R.font.roboto_regular)
 
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
@@ -24,11 +32,23 @@ abstract class SwipeGesture(context : Context) : ItemTouchHelper.SimpleCallback(
     }
 
     override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+
+
         RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
             .addSwipeLeftBackgroundColor(deleteColor)
+            .addSwipeLeftLabel("Delete")
             .addSwipeLeftActionIcon(deleteIcon)
+
             .addSwipeRightBackgroundColor(recapColor)
+            .addSwipeRightLabel("Done")
             .addSwipeRightActionIcon(recapIcon)
+
+            .setSwipeLeftLabelTextSize(TypedValue.COMPLEX_UNIT_SP, 16F)
+            .setSwipeRightLabelTextSize(TypedValue.COMPLEX_UNIT_SP, 16F)
+
+            .setSwipeLeftLabelTypeface(typeface)
+            .setSwipeRightLabelTypeface(typeface)
+
             .create()
             .decorate()
 
