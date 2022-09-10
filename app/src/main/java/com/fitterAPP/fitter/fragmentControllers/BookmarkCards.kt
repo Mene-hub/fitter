498370efbf6563070103dc025fdaeb959946315e
Code-Dani <satrianodaniel@gmail.com>
2022-09-10 12:28:59 +0200

    Eliminate ref non usate

--- app/src/main/java/com/fitterAPP/fitter/fragmentControllers/BookmarkCards.kt
+++ app/src/main/java/com/fitterAPP/fitter/fragmentControllers/BookmarkCards.kt
@@ -1,30 +1,14 @@
 package com.fitterAPP.fitter.fragmentControllers
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.view.isGone
-import com.denzcoskun.imageslider.ImageSlider
-import com.denzcoskun.imageslider.constants.ScaleTypes
-import com.denzcoskun.imageslider.models.SlideModel
 import com.fitterAPP.fitter.MainActivity
-import com.fitterAPP.fitter.R
 import com.fitterAPP.fitter.classes.BookmarkCard
-import com.fitterAPP.fitter.classes.Exercise
-import com.fitterAPP.fitter.classes.ExerciseQueryHelper
 import com.fitterAPP.fitter.databinding.FragmentBookmarkCardsBinding
 import com.fitterAPP.fitter.itemsAdapter.BookmarkAdapter
-import com.fitterAPP.fitter.itemsAdapter.FitnessCardExercisesAdapter
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 /**
  * Fragment which manage the recycler view inside the layout
